a4ef48cfecb4437c0ed31a0758fd7f9e2e457461
hiraPince <LI56211100883284@163.com>
2021-03-10 08:44:43 +0800

    更新 5.4 17.cpp

--- 5.4 17.cpp
+++ 5.4 17.cpp
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	int a[2][3]={{1,2,3},{1,2,3}};
-	int b[3][2]={{1,2},{1,2},{1,2}};
-	int v[2][2]={};
+	int a[4][3]={{1,2,3},{1,2,3},{1,2,3},{1,2,3}};
+	int b[3][4]={{1,2,3,4},{1,2,3,4},{1,2,3,4}};
+	int v[4][4]={};
 	int i,j,m;
-	for(i=0;i<2;i++)
-		for(m=0;m<2;m++)
+	for(i=0;i<4;i++)
+		for(m=0;m<4;m++)
 		{
 			int sum=0;
 			for(j=0;j<3;j++)
 				sum+=a[i][j]*b[j][m];
 			v[i][m]=sum;
 		}
-	for(i=0;i<2;i++)
-		for(m=0;m<2;m++)
+	for(i=0;i<4;i++)
+		for(m=0;m<4;m++)
 			printf("%d\n",v[i][m]);
 	return 0;	
 	
