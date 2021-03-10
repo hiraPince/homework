f10044457ad87720ed84fd93ad57447002ad7bd8
hiraPince <LI56211100883284@163.com>
2021-03-10 14:41:33 +0800

    更新 5.4(6).cpp

--- 5.4(6).cpp
+++ 5.4(6).cpp
@@ -2,15 +2,14 @@
 int main()
 {
 	double a[10];
-	double n=0.0,m=0.0;
+	double n=0.0;
 	int i;
 	for(i=0;i<10;i++)
 	scanf("%lf",&a[i]);
 	for(i=0;i<10;i++)
 	{
-		n=1/a[i];
-		m+=n;
+		n+=1/a[i];
 	}		
-	printf("%lf",m);
+	printf("%lf",n);
 	return 0;	
 }
