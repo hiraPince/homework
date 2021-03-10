49f8d4c097ccc5d24543f8416d089a4c1cbd3f5f
hiraPince <LI56211100883284@163.com>
2021-03-10 09:08:41 +0800

    更新 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,13 +3,15 @@
 void CalculateRealRoot(double a1,double b1,double c1);
 int main(void)
 {
-	double a,b,c;
+	double a,b,c,d;
 	printf("please input a:");
 	scanf("%lf",&a);
 	printf("please input b:");
 	scanf("%lf",&b);
 	printf("please input c:");
 	scanf("%lf",&c);
-	CalculateRealRoot(a,b,c);
+	printf("please input d:");
+	scanf("%lf",&d)
+	CalculateRealRoot(a,b,c,d);
 	return 0;
 }
