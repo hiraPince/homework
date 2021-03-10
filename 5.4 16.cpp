d3eed44c2489365884510ec74d56513cd1ec463c
hiraPince <LI56211100883284@163.com>
2021-03-10 08:41:58 +0800

    更新 5.4 16.cpp

--- 5.4 16.cpp
+++ 5.4 16.cpp
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	int a[2][3];
-	int b[3][2]; 
+	int a[4][3];
+	int b[3][4]; 
 	int i,j;
-	for(i=0;i<2;i++)
+	for(i=0;i<4;i++)
 		for(j=0;j<3;j++)
 		{
 			scanf("%d",&a[i][j]);
 		}
-		for(i=0;i<2;i++)
+		for(i=0;i<4;i++)
 			for(j=0;j<3;j++)
 				b[j][i]=a[i][j];
 		
 		for(j=0;j<3;j++)
-			for(i=0;i<2;i++)
+			for(i=0;i<4;i++)
 			printf("%d\n",b[j][i]);
 	return 0;
  } 
