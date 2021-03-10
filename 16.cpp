0f01389dae8a1647ecb1196fe70235cee950fd31
hiraPince <LI56211100883284@163.com>
2021-03-10 14:36:40 +0800

    更新 16.cpp

--- 16.cpp
+++ 16.cpp
@@ -4,12 +4,12 @@ int main(void)
 	int a,b,c,d,e,f,g,h;
     a=1;
 	b=2;
-	c=a>0&&!b!=0;
-	d=a>0,b>0;
-	e=a>>b%32;
-	f=a&b;
-	g=a*=b;
-	h=sizeof(a);
+	c=-a;
+	d=-b;
+	e=a+b;
+	f=a/b;
+	g=a%b;
+	h=a>=b;
 	printf("c=%d d=%d e=%d f=%d g=%d h=%d\n",c,d,e,f,g,h);
 	
 	return 0;
