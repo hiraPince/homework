4f00909b1d93b9d34e42cdb73cd3f2663936edf4
hiraPince <LI56211100883284@163.com>
2021-03-10 14:37:58 +0800

    更新 17.cpp

--- 17.cpp
+++ 17.cpp
@@ -4,11 +4,11 @@ int main(void)
 	double a,b,c,d,e,h,j;
 	a=1.0;
 	b=2.0;
-	c=a>=b;
-	d=a>0&&!b!=0;
-	e=a>0,b>0;
-	h=a*=b;
-	j=sizeof(a);
+	c=-a;
+	d=-b;
+	e=a+b;
+	h=a/b;
+	j=a%b;
 	printf("c=%lf d=%lf e=%lf h=%lf j=%lf\n",c,d,e,h,j);
 	return 0; 
 }
